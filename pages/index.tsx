d77749c6a09c700c7fb1fa01c0f3ea7bbb330e94
Mlem <ngsinh4@gmail.com>
2021-07-16 22:22:52 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          </span> 
         </a>
       </footer>
     </div>
